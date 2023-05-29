6571082c99a7ae1cdb5f625fd9335df87a2d017b
gyuri <m25152515@gmail.com>
2023-05-29 16:57:15 +0900

    Feat:layout

--- app/src/main/java/com/example/mobilecalendar/MainActivity.kt
+++ app/src/main/java/com/example/mobilecalendar/MainActivity.kt
@@ -1,14 +1,29 @@
-package com.example.mobilecalendar
 
-import androidx.appcompat.app.AppCompatActivity
+
+
+import android.content.Intent
 import android.os.Bundle
+import android.widget.CalendarView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mobilecalendar.databinding.ActivityMainBinding
+import com.example.mobilecalendar.databinding.TodoBinding
+
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.calendar.setOnClickListener{
+            val intent = Intent(this , CalendarView::class.java)
+            startActivity(intent)
+        }
+        binding.todo.setOnClickListener {
+            val intent = Intent(this, TodoBinding::class.java)
+            startActivity(intent)
+        }
     }
 }
