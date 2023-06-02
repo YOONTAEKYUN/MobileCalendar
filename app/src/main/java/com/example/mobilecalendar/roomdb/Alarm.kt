994fb93adf230cdca985b9d8a3bc2dbb35fd1e0f
tk.yoon <tk.yoon0105@gmail.com>
2023-06-02 17:18:48 +0900

    feat: scheduleId 인덱싱 설정 추가

--- app/src/main/java/com/example/mobilecalendar/roomdb/Alarm.kt
+++ app/src/main/java/com/example/mobilecalendar/roomdb/Alarm.kt
@@ -2,6 +2,7 @@ package com.example.mobilecalendar.roomdb
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.time.LocalTime
 
@@ -13,7 +14,7 @@ import java.time.LocalTime
             childColumns = ["scheduleId"],
             onDelete = ForeignKey.CASCADE
         )
-    ]
+    ], indices = [Index(value = ["scheduleId"])]
 )
 
 data class Alarm(
