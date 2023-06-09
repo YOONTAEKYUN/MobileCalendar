17fdd20ba0693d4c504ecb619d616dd4ac770d7b
gyuri <m25152515@gmail.com>
2023-06-10 07:17:51 +0900

    feat: calendar layout

--- app/src/main/java/com/example/mobilecalendar/DayViewContainer.kt
+++ app/src/main/java/com/example/mobilecalendar/DayViewContainer.kt
@@ -1,13 +1,15 @@
 package com.example.mobilecalendar
 
 import android.view.View
-import android.widget.TextView
 import com.example.mobilecalendar.databinding.CalendarDayLayoutBinding
 import com.kizitonwose.calendar.view.ViewContainer
+
 
 class DayViewContainer(view: View) : ViewContainer(view) {
     //val textView = view.findViewById<TextView>(R.id.calendarDayText)
 
     // With ViewBinding
     val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
+
 }
+
---
+++ app/src/main/java/com/example/mobilecalendar/WeekFrag.kt
@@ -0,0 +1,73 @@
+package com.example.mobilecalendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.children
+import androidx.fragment.app.Fragment
+import com.example.mobilecalendar.databinding.WeekLayoutBinding
+import com.kizitonwose.calendar.core.*
+import com.kizitonwose.calendar.view.WeekDayBinder
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.*
+
+
+class WeekFrag : Fragment() {
+    private lateinit var binding: WeekLayoutBinding
+
+    private val selectedDates = mutableSetOf<LocalDate>()
+
+    private fun dateClicked(date: LocalDate) {
+        if (selectedDates.contains(date)) {
+            selectedDates.remove(date)
+        } else {
+            selectedDates.add(date)
+        }
+        // Refresh both calendar views..
+        binding.weekCalendarView.notifyDateChanged(date)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = WeekLayoutBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.weekCalendarView.dayBinder = object : WeekDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+
+            override fun bind(container: DayViewContainer, data: WeekDay) {
+                container.textView.text = data.date.dayOfMonth.toString()
+                view.setOnClickListener {
+                    if (data.position == WeekDayPosition.RangeDate) {
+                        dateClicked(date = data.date)
+                    }
+                }
+            }
+        }
+        val daysOfWeek = daysOfWeek()
+        binding.titlesContainer2.root.children
+            .map { it as TextView }
+            .forEachIndexed { index, textView ->
+                val dayOfWeek = daysOfWeek[index]
+                val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                textView.text = title
+
+            }
+        val currentDate = LocalDate.now()
+        val currentMonth = YearMonth.now()
+        val startDate = currentMonth.minusMonths(100).atStartOfMonth() // Adjust as needed
+        val endDate = currentMonth.plusMonths(100).atEndOfMonth()  // Adjust as needed
+        val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
+        binding.weekCalendarView.setup(startDate, endDate, firstDayOfWeek)
+        binding.weekCalendarView.scrollToWeek(currentDate)
+
+        return view
+    }
+}
